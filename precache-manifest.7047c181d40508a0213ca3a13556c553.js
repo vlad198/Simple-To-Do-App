7935d308d37c870813c52a09984c8377e69733d6
Vlad <vladalin123456@gmail.com>
2019-02-07 15:49:38 +0200

    Updates

---
+++ precache-manifest.7047c181d40508a0213ca3a13556c553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0118874ebc79ec56901",
+    "url": "//Simple-To-Do-App/static/js/runtime~main.a0118874.js"
+  },
+  {
+    "revision": "44b314d276139a1775e1",
+    "url": "//Simple-To-Do-App/static/js/main.44b314d2.chunk.js"
+  },
+  {
+    "revision": "b324660b55572193a7c5",
+    "url": "//Simple-To-Do-App/static/js/1.b324660b.chunk.js"
+  },
+  {
+    "revision": "44b314d276139a1775e1",
+    "url": "//Simple-To-Do-App/static/css/main.bf7911a3.chunk.css"
+  },
+  {
+    "revision": "293555d2c5b480efd6376177dad6ea5b",
+    "url": "//Simple-To-Do-App/index.html"
+  }
+];
